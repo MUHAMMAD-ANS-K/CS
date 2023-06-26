435c519b75354643f44be268a002bbc80948267f
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-06-26 20:18:51 +0000

    Tue, Jun 27, 2023, 1:18 AM +05:00

--- loops.c
+++ loops.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    st
+   const int n = 5;
+   for(int i = 0; i < n; i++)
+   {
+    printf("#");
+    for(int j = 0; j < n; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+   }
+   int c;
+   do
+   {
+        printf("||");
+        c++;
+   }
+   while(c < 3);
 }
