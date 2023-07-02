b86c52155fbd2106035f963c6d5489bc6692e023
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-02 20:48:50 +0000

    Mon, Jul 3, 2023, 1:48 AM +05:00

--- loop.c
+++ loop.c
@@ -9,10 +9,11 @@ int main(void)
         c = get_int("Size: \n")
     }
     while(c < 0);
-     int c;
-   do
-   {
-        c = get_int("Size: ");
-   }
-   while(c < 0);
+     for(int i = 0; i < c; i++)
+     {
+        for(int j = 0;j < c;j++)
+        {
+            printf("#")
+        }
+     }
 }
