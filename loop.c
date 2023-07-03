c38b2f7e1e42a27eaa19d4014d07032768bbcdf7
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-03 11:02:05 +0000

    Mon, Jul 3, 2023, 4:02 PM +05:00

--- loop.c
+++ loop.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
+int get_size(void);
+void printgrid(size);
 
 int main(void)
 {
@@ -8,8 +10,25 @@ int main(void)
 }
 
 
-int get_size = int c;
-
+int get_size(void)
+{
+    int c;
+    do
+    {
+        c = get_int("Size: ");
+    }
+    while(c < 0);
+}
+void printgrid(size)
+{
+    for(i = 0;i < c;i++)
+    {
+        for(j = 0;j < c;j++)
+        {
+            printf("#");
+        }printf("\n");
+    }
+}
 
 
 
