cb7844a6c69731797899463e033cbdd1a8adc642
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-03 11:13:03 +0000

    Mon, Jul 3, 2023, 4:13 PM +05:00

--- calculator.c
+++ calculator.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    int x = get_int("x: \n");
-    int y = get_int("y: \n");
-    printf("%i",x + y);
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+    printf("%i\n",x * y);
 }
