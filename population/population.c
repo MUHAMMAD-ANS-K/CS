462d79c7092c917a23c9f27bf9580055a0adacd7
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-08 12:29:02 +0000

    Sat, Jul 8, 2023, 5:29 PM +05:00

--- population/population.c
+++ population/population.c
@@ -3,24 +3,27 @@
 
 int main(void)
 {
+    //This part prompts the user for starting number of llamas and reprompts in case of invalid input.
     int s;
     do
     {
         s = get_int("Starting number of llamas: ");
     }
-    while(s < 9);
+    while (s < 9);
+    //This part prompts the user for ending number of llamas and reprompts in case of invalid input.
     int g;
     do
     {
         g = get_int("Goal number of llamas: ");
 
     }
-    while(g < s);
+    while (g < s);
+    //This part calculates year to reach goal number of llamas and prints them to the screen.
     int years = 0;
-    while(s < g)
+    while (s < g)
     {
-        s = s + (s/3) - (s/4);
+        s = s + (s / 3) - (s / 4);
         years++;
     }
-    printf("Years: %i\n",years);
+    printf("Years: %i\n", years);
 }
