0032aa34702fa4f8d374c6100f74e10ae41dab46
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-08 15:25:37 +0000

    Sat, Jul 8, 2023, 8:25 PM +05:00

--- mario-more/rf.c
+++ mario-more/rf.c
@@ -4,8 +4,11 @@
 int main (void)
 {
     int h = get_int("Height: ");
-    for(i = 0; i < h; i++)
+    for(int i = 0; i < h; i++)
     {
-
+        for(int j = h; j > i;j--)
+        {
+            printf("#");
+        }
     }
 }
