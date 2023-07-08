2e7ec7050ccf7d97d018d14ea376238ec93a2b80
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-08 15:09:11 +0000

    Sat, Jul 8, 2023, 8:09 PM +05:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,7 +11,7 @@ int main(void)
     while (h > 8 || h < 2);
     for(int i = 0; i < h; i++)
     {
-        for(int j = 0;j < i; j++)
+        for(int j = 0;j <= i; j++)
         {
             printf("# ");
         }
