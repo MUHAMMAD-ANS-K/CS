fe6fe4673075b79dbb7689a8af2d902aa65d81cc
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-09 13:41:53 +0000

    Sun, Jul 9, 2023, 6:41 PM +05:00

--- s.c
+++ s.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
     string s = get_string("Name: ");
-    printf("%i %i %i \n", s[0], s[1] ,s[2]);
+    printf("%i\n", sterlin(s));
 }
