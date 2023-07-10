cc91b7a55f80c5624d83c1f7fa8b991f49734b84
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-10 19:42:45 +0000

    Tue, Jul 11, 2023, 12:42 AM +05:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,7 +5,11 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-for(i = 0; i )
+int 
+for(i = 0; i < 26; i++)
+{
+    int
+}
 
 int compute_score(string word);
 
