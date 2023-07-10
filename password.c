cf1c50be29f3ede6a80742f21ac79261ee07e361
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-10 19:09:25 +0000

    Tue, Jul 11, 2023, 12:09 AM +05:00

--- password.c
+++ password.c
@@ -24,7 +24,7 @@ bool valid(string password)
 {
     for(int i = 0; i < strlen(password); i++)
     {
-       if(ispunct(password[i]))
+       if(ispunct(password[i]) && islower(password[i]) && isupper(password[i]) && isdigit(password[i]))
        {
         return true;
        }
