0ad32a89974631ff371e749b289995f5e5bee93f
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-10 19:25:38 +0000

    Tue, Jul 11, 2023, 12:25 AM +05:00

--- password.c
+++ password.c
@@ -19,17 +19,19 @@ int main(void)
     }
 }
 //islower(password[i]) && isupper(password[i]) && isdigit(password[i])
-//if(islower(password[i]))
-      // {
-       //}
+
 // TODO: Complete the Boolean function below
 bool valid(string password)
 {
     for(int i = 0; i < strlen(password); i++)
     {
-           for(int i = 0; i < strlen(password); i++)
+           for(int k = 0; k < strlen(password); k++)
     {
-       if(ispunct(password[i]))
+       if(ispunct(password[k]) )
+       {
+        return true;
+       }
+       if(islower(password[i]))
        {
         return true;
        }
