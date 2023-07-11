dcc1f1a3f76ac79daf6496c4246c4912276ed25a
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-11 11:46:19 +0000

    Tue, Jul 11, 2023, 4:46 PM +05:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+int count_letters(string txt);
 
 int main(void)
 {
     string text = get_string("Text: ");
+    printf("%i\n", count_letters(text));
+}
+
+
+int count_letters(string txt)
+{
+    int ltrs = strlen(txt);
+    return ltrs;
 }
