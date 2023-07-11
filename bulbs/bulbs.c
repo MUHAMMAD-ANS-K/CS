74ef8607e9d2b5195792728b7fd5802516de8479
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-11 19:17:18 +0000

    Wed, Jul 12, 2023, 12:17 AM +05:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -1,8 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-#include <cs50.h>
-#include <stdlib.h>
 
 const int BITS_IN_BYTE = 8;
 
@@ -13,10 +11,10 @@ int main(void)
     int bits[8];
     string text = get_string("Message: ");
     int number[strlen(text)];
-    for(int i = 0; i < strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
         number[i] = text[i];
-        for(int j = 7; j >= 0; j--)
+        for (int j = 7; j >= 0; j--)
         {
             bits[j] = number[i] % 2;
             number[i] = number[i] / 2;
@@ -27,24 +25,18 @@ int main(void)
 
 void print_bulb(int bit[BITS_IN_BYTE])
 {
-    for(int i = 0; i < BITS_IN_BYTE; i++)
+    for (int i = 0; i < BITS_IN_BYTE; i++)
     {
-    if (bit[i] == 0)
-    {
-        // Dark emoji
-        printf("\U000026AB");
-    }
-    else if (bit[i] == 1)
-    {
-        // Light emoji
-        printf("\U0001F7E1");
-    }
+        if (bit[i] == 0)
+        {
+            // Dark emoji
+            printf("\U000026AB");
+        }
+        else if (bit[i] == 1)
+        {
+            // Light emoji
+            printf("\U0001F7E1");
+        }
     }
     printf("\n");
 }
-
-        //while(number[i] > 0)
-        //{
-          //  bits[i] = number[i] % 2;
-        //}
-        //printf("%i", bits[i]);
