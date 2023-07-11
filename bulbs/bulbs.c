60ec1142b35933fef324596a613b959fe04c16f8
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-11 18:07:19 +0000

    Tue, Jul 11, 2023, 11:07 PM +05:00

---
+++ bulbs/bulbs.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+#include <stdlib.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    int bits[8];
+    string text = get_string("Message: ");
+    int number[strlen(text)];
+    for(int i = 0; i < strlen(text); i++)
+    {
+        number[i] = text[i];
+
+    }
+    printf("%i\n", number[0]);
+
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
+
+        //while(number[i] > 0)
+        //{
+          //  bits[i] = number[i] % 2;
+        //}
+        //printf("%i", bits[i]);
