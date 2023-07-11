c510e89af7363d97d606afd05620c1d9d97363ab
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-11 11:50:17 +0000

    Tue, Jul 11, 2023, 4:50 PM +05:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 int count_letters(string txt);
 
 int main(void)
@@ -12,6 +13,6 @@ int main(void)
 
 int count_letters(string txt)
 {
-    int ltrs = strlen(txt);
+    int ltrs = strlen(isalpha(txt));
     return ltrs;
 }
