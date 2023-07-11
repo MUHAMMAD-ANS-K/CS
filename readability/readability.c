1f1f6ffd7c8a504676b459cfcb649b5e74f4aea0
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-11 12:05:14 +0000

    Tue, Jul 11, 2023, 5:05 PM +05:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,11 +4,12 @@
 #include <ctype.h>
 int count_letters(string txt);
 int count_words(string txt);
+int count_sentences(string txt);
 
 int main(void)
 {
     string text = get_string("Text: ");
-    printf("%i   %i\n", count_letters(text), count_words(text));
+    printf("%i   %i   %i\n", count_letters(text), count_words(text), count_sentences(text));
 }
 
 
