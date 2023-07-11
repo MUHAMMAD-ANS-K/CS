fefd50dc0102c5d8356f8d53d71ec407fdb5cc5a
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-11 14:56:59 +0000

    Tue, Jul 11, 2023, 7:56 PM +05:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,19 +11,19 @@ int main(void)
 {
     string text = get_string("Text: ");
 
-    float L = (count_letters(text)/(float) count_words(text)) * 100;
-    float S = (count_sentences(text)/(float) count_words(text)) * 100;
+    float L = (count_letters(text) / (float) count_words(text)) * 100;
+    float S = (count_sentences(text) / (float) count_words(text)) * 100;
     float ind = 0.0588 * L - 0.296 * S - 15.8;
     int index = round(ind);
-    if(index < 1)
+    if (index < 1)
     {
         printf("Before Grade 1\n");
     }
-    else if(index < 16)
+    else if (index < 16)
     {
-        printf("Grade %i\n",index);
+        printf("Grade %i\n", index);
     }
-    else if(index >= 16)
+    else if (index >= 16)
     {
         printf("Grade 16+\n");
     }
@@ -34,9 +34,9 @@ int main(void)
 int count_letters(string txt)
 {
     int ltrs = 0;
-    for(int i = 0; i < strlen(txt); i++)
+    for (int i = 0; i < strlen(txt); i++)
     {
-        if(isalpha(txt[i]))
+        if (isalpha(txt[i]))
         {
             ltrs++;
         }
@@ -48,9 +48,9 @@ int count_letters(string txt)
 int count_words(string txt)
 {
     int wrd = 1;
-    for(int k = 0; k < strlen(txt); k++)
+    for (int k = 0; k < strlen(txt); k++)
     {
-        if(isspace(txt[k]))
+        if (isspace(txt[k]))
         {
             wrd++;
         }
@@ -61,9 +61,9 @@ int count_words(string txt)
 int count_sentences(string txt)
 {
     int sntns = 0;
-    for(int j = 0; j < strlen(txt); j++)
+    for (int j = 0; j < strlen(txt); j++)
     {
-        if(txt[j] == '!' || txt[j] == '.' || txt[j] == '?')
+        if (txt[j] == '!' || txt[j] == '.' || txt[j] == '?')
         {
             sntns++;
         }
