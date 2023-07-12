4e69ea2e6225d2a3c8a5c9ab996628ec42ac295b
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-12 11:51:46 +0000

    Wed, Jul 12, 2023, 4:51 PM +05:00

--- cash/cash.c
+++ cash/cash.c
@@ -44,8 +44,12 @@ int get_cents(void)
 int calculate_quarters(int cents)
 {
     int quarters = 0;
-    
-    return 0;
+    while(cents > 24)
+    {
+        cents - 25;
+        quarters++;
+    }
+    return quarters;
 }
 
 int calculate_dimes(int cents)
