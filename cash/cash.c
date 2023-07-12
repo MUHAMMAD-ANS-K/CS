115da39270771ca3265f30e90247f29e5bc3b9eb
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-12 13:53:12 +0000

    Wed, Jul 12, 2023, 6:53 PM +05:00

--- cash/cash.c
+++ cash/cash.c
@@ -37,7 +37,12 @@ int main(void)
 
 int get_cents(void)
 {
-    int cents = get_int("Cents: ");
+    int cents;
+    do
+    {
+        cents = get_int("Cents: ");
+    }
+    while(cents < 0);
     return cents;
 }
 
