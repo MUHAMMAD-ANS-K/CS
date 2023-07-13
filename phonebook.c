ae34b8aa3dc97e9c5aa1b6073532dbd8bcd15696
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-13 15:03:34 +0000

    Thu, Jul 13, 2023, 8:03 PM +05:00

---
+++ phonebook.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+const int n = 2;
+
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+int main(void)
+{
+    person people[n];
+    people[0].name = "David";
+    people[0].number = "+1-876-543-2109";
+    people[1].name = "Carter";
+    people[1].number = "+1-543-219-8765";
+    string s = get_string("Name: ");
+    for(i = 0; i < n; i++)
+    {
+        if(strcmp(people[i].name , s) == 0)
+        {
+            printf("Found: %i", people[i].number);
+        }
+    }
+    if(strcmp(people[i].name , s) =! 0)
+    {
+        printf("Not Found");
+    }
+}
