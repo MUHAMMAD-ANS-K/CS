cb88ea128ee2fd0d0a8673d6a895d0a679972ec8
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-17 19:31:42 +0000

    Tue, Jul 18, 2023, 12:31 AM +05:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -3,16 +3,6 @@
 
 /**
  * Beach Burger Shack has the following 10 items on their menu
- * Burger: $9.5
- * Vegan Burger: $11
- * Hot Dog: $5
- * Cheese Dog: $7
- * Fries: $5
- * Cheese Fries: $6
- * Cold Pressed Juice: $7
- * Cold Brew: $3
- * Water: $2
- * Soda: $2
 */
 
 #include <cs50.h>
@@ -74,7 +64,26 @@ int main(void)
 // Add at least the first four items to the menu array
 void add_items(void)
 {
-    return;
+    menu[0].item = "Burger";
+    menu[0].price = 9.50;
+    menu[1].item = "Vegan Burger";
+    menu[1].price = 11.00;
+    menu[2].item = "Hot Dog";
+    menu[2].price = 5.00;
+    menu[3].item = "Cheese Dog";
+    menu[3].price = 7.00;
+    menu[4].item = "Fries";
+    menu[4].price = 5.00;
+    menu[5].item = "Cheese Fries";
+    menu[5].price = 6.00;
+    menu[6].item = "Cold Pressed Juice";
+    menu[6].price = 7.00;
+    menu[7].item = "Cold Brew";
+    menu[7].price = 3.00;
+    menu[8].item = "Water";
+    menu[8].price = 2.00;
+    menu[9].item = "Soda";
+    menu[9].price = 2.00;
 }
 
 // Search through the menu array to find an item's cost
