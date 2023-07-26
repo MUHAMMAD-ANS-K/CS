883f4ebac6eec1a9b3d9fb41d5e138a7048c9014
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-26 18:04:27 +0000

    Wed, Jul 26, 2023, 11:04 PM +05:00

--- checkpdf.c
+++ checkpdf.c
@@ -1,2 +1,14 @@
-#include <stdio>
-#include <stdint>
+#include <stdio.h>
+#include <stdint.h>
+#include <cs50.h>
+int main(int argc , string argv[])
+{
+    if(argc != 2)
+    {
+        printf("Invalid Input\n");
+        return 1;
+    }
+    string filename = argv[1];
+    FILE *file = fopen(filename , "r");
+    fclose(file);
+}
