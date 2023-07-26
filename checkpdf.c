7b5434578af3c24f3ca1f886531609b1a9a69e5b
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-26 18:13:39 +0000

    Wed, Jul 26, 2023, 11:13 PM +05:00

--- checkpdf.c
+++ checkpdf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <cs50.h>
+
+int sizeof_buffer = 4;
 int main(int argc , string argv[])
 {
     if(argc != 2)
@@ -15,7 +17,19 @@ int main(int argc , string argv[])
         printf("File not Found\n");
         return 0;
     }
-    uint8_t buffer[4];
+    uint8_t pdf_digits[] = {37, 80, 68, 70};
+    uint8_t buffer[sizeof_buffer];
     fread(buffer ,1 ,4 ,file);
+    for(int i = 0; i < sizeof_buffer; i++)
+    {
+        if(buffer[i] != pdf_digits[i])
+        {
+            printf("File is likely not a pdf one\n");
+            fclose(file);
+            return 0;
+        }
+    }
+    printf("Looks like a pdf\n");
     fclose(file);
+    return 0;
 }
