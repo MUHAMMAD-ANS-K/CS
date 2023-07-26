843651b9d94af1b3a8d30a37069c939e785caacb
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-26 18:06:45 +0000

    Wed, Jul 26, 2023, 11:06 PM +05:00

--- checkpdf.c
+++ checkpdf.c
@@ -10,5 +10,12 @@ int main(int argc , string argv[])
     }
     string filename = argv[1];
     FILE *file = fopen(filename , "r");
+    if(file == NULL)
+    {
+        printf("File not Found\n");
+        return 0;
+    }
+    uint8_t buffer[4];
+    fread(buffer ,1 ,4 ,file);
     fclose(file);
 }
