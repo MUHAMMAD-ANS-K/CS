4ded0b4cd70941566ee0fe376a46573815d852e6
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-27 15:22:18 +0000

    Thu, Jul 27, 2023, 8:22 PM +05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,7 +128,10 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    if(strcmp())
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if(strcmp(name , ))
+    }
     return false;
 }
 
