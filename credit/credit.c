e7efd1584273aa0b65f99783f783f340f224516f
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-27 11:32:13 +0000

    Thu, Jul 27, 2023, 4:32 PM +05:00

--- credit/credit.c
+++ credit/credit.c
@@ -13,21 +13,21 @@ int main(void)
     }
     int sum = 0;
     int num[n_length];
-    for (int i = n_length - 1; i >= 0 ; i--)
+    for (int i = n_length - 1; i >= 0; i--)
     {
         num[i] = number % 10;
-        number = (number - num[i])/10;
+        number = (number - num[i]) / 10;
     }
     int y;
     int x = n_length - 2;
     while (x >= 0)
     {
         int z = num[x] * 2;
-        if(z > 9)
+        if (z > 9)
         {
-           y =  z % 10;
-           sum += y;
-           z = (z - y) / 10;
+            y = z % 10;
+            sum += y;
+            z = (z - y) / 10;
         }
         sum += z;
         sum += num[x + 1];
@@ -52,23 +52,12 @@ int main(void)
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 int number_length(long number)
 {
     int count = 0;
     while (number > 0)
     {
-       number =  number/10;
+        number = number / 10;
         count += 1;
     }
     return count;
