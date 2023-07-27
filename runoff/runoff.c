92a1e82e95a088943bc3a88f4ec307f8d34580da
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-27 18:56:51 +0000

    Thu, Jul 27, 2023, 11:56 PM +05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -174,9 +174,13 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
+    int min;
     for(int i = 0; i < candidate_count; i++)
     {
-        if(candidates[i].eliminated)
+        if(!candidates[i].eliminated)
+        {
+            
+        }
     }
     return 0;
 }
