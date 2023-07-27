58a725aa487e17e757f4799363ecb06c6f8a3cd4
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-27 16:54:06 +0000

    Thu, Jul 27, 2023, 9:54 PM +05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -141,12 +141,14 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for(int i = 0; i < )
-    if(candidate.eliminated == false)
+    for(int i = 0; i < candidate_count; i ++)
     {
+        if(candidates[i].eliminated == false && )
+        {
 
+        }
     }
-    return;
+     return;
 }
 
 // Print the winner of the election, if there is one
