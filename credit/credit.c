ec7ab2c4e6cd938d0fb8c7fffc9b1b636315f542
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-27 10:10:35 +0000

    Thu, Jul 27, 2023, 3:10 PM +05:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,15 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
+int number_length(long number);
 
 int main(void)
 {
     long number = get_long("Number: ");
-    int num = number % 10;
-    number = (number - num)/10;
-        int num = number % 10;
-    number = (number - num)/10;    int num = number % 10;
-    number = (number - 
-    int sum = 0;
-    printf("%i\n", sum);
+    int num;
+    int n_length = number_length(number);
 
+   // for(int )
+    //number = (number - num)/10;
+    int sum = 0;
+    printf("%i\n", n_length);
+
+}
+
+int number_length(long number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+       number =  number/10;
+        count += 1;
+    }
+    return count;
 }
