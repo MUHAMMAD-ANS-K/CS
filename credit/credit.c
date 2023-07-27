b8d968d748ed83adaa6409749b8358bd0984f882
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-07-27 09:50:30 +0000

    Thu, Jul 27, 2023, 2:50 PM +05:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,10 +6,10 @@ int main(void)
     long number = get_long("Number: ");
     int num;
     int sum = 0;
-    while(num > 0)
+    while(number > 0)
     {
         num = number % 100;
-        (number - num)/10;
+        number = (number - num)/100;
         sum += num*2;
     }
     printf("%i\n", sum);
