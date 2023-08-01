264fc8074e173ca3fea878752c3eb298c5502b85
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-01 22:27:54 +0000

    Wed, Aug 2, 2023, 3:27 AM +05:00

--- harry.py
+++ harry.py
@@ -1,6 +1,8 @@
 di = [
-    {'name': 'Ginny', 'house' : '' , 'patronus' : ''},
-     {'name': 'Hermione', 'house' : '' , 'patronus' : 'Otter'},
-      {'name': 'Harry', 'house' : '' , 'patronus' : 'Stag'},
-       {'name': '', 'house' : '' , 'patronus' : ''}
+    {'name': 'Ginny', 'house' : 'Gryffindor' , 'patronus' : 'Horse'},
+     {'name': 'Hermione', 'house' : 'Gryffindor' , 'patronus' : 'Otter'},
+      {'name': 'Harry', 'house' : 'Gryffindor' , 'patronus' : 'Stag'},
+       {'name': 'Draco Malfoy', 'house' : 'Slytherin' , 'patronus' : None}
 ]
+for student in di:
+    for name in student:
