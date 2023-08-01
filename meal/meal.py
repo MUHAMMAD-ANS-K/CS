52ec5b3c12cb350679cf1f6c116853a680c63e5d
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-01 17:01:39 +0000

    Tue, Aug 1, 2023, 10:01 PM +05:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,15 @@
 def main():
     t = input('What time is it?')
+    print(convert(t))
 
 
 def convert(time):
-    hours, minutes = split(':')
+    hours, minutes = float(split(':'))
+    minutes = minutes / 60
+    hours = round(hours + minutes, 1)
+    return hours
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+
+main()
