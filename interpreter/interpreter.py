39e5125c664f4f3961bc9301e5c75f896649a44a
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-01 16:50:47 +0000

    Tue, Aug 1, 2023, 9:50 PM +05:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,10 +1,10 @@
 expression = input('Expression: ')
 x, y , z = expression.split()
 if y.startswith('+'):
-    print(float(x) + float(z))
+    print(f'{float(x) + float(z) : .1f}')
 elif y.startswith('-'):
-    print(float(x) - float(z))
+    print(f'{float(x) - float(z) : .1f}')
 elif y.startswith('*'):
-    print(float(x) * float(z))
+    print(f'{float(x) * float(z) : .1f}')
 elif y.startswith('/'):
-    print(f'{float(x) / float(z)}')
+    print(f'{float(x) / float(z) : .1f}')
