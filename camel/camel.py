a05e29b91741d30fc02d4d6e9d08d3cffdefa035
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-01 20:00:25 +0000

    Wed, Aug 2, 2023, 1:00 AM +05:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,5 +1,8 @@
 def main():
     camelCase = input('camelCase: ')
+    print('snake_case: ',end = '')
     for i in camelCase:
-        print(f'_{i.lower()}') if i.isupper() else print(i)
+        print(f'_{i.lower()}', end = '') if i.isupper() else print(i, end ='')
+    print()
+
 main()
