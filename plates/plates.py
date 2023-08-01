80bad888cef30c071b5f7f68a76c18f1814ebdd1
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-01 21:15:57 +0000

    Wed, Aug 2, 2023, 2:15 AM +05:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,21 +7,19 @@ def main():
 
 
 def is_valid(s):
-    if 7 > len(s) > 1:
-        r_one = True
-    else:
+    if !(7 > len(s) > 1):
         return False
     if s[0].isalpha() and s[1].isalpha():
         r_two = True
+    else:
+        return False
 
     r_three = True
     for i in range(len(s)):
         if s[i].isdecimal():
             while i > len(s):
                 if s[i].isalpha():
-                    r_three = false
-                    break
+                    return False
                 i += 1
-    if r_one == True and r_two == True and r_three == True:
-        return True
+    return True
 main()
