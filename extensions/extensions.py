99fb8d963a9858fdfef04b9e01e748b35e52b3eb
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-01 16:15:13 +0000

    Tue, Aug 1, 2023, 9:15 PM +05:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,6 +1,6 @@
 
 def main():
-    filename = input('File: ').casefold()
+    filename = input('File: ').casefold().strip()
     if filename.endswith('.gif'):
         print('image/gif')
     elif filename.endswith('.jpg') or filename.endswith('.jpeg'):
