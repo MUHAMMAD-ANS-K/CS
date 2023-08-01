ca49cee78c13855ae345cc989aacd6a6bbc1be85
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-01 20:33:24 +0000

    Wed, Aug 2, 2023, 1:33 AM +05:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,10 @@
 def main():
     input = input('Input: ')
     output = input.lower()
+    j = 0
     for i in output:
-        match i:
-            case a:
+        if i != 'a' or i != 'e' or i != 'i' or i != 'o' or i != 'u':
+            print(input[j])
+        j += 1
+
+main()
