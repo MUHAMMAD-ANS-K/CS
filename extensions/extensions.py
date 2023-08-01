699d848395e670c6239716cc4d7cf9831042f8ec
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-01 16:02:53 +0000

    Tue, Aug 1, 2023, 9:02 PM +05:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,7 +1,21 @@
 
 def main():
     filename = input('File: ')
-    if filename.endswith(''):
-        print('')
+    if filename.endswith('.gif'):
+        print('image/gif')
+    elif filename.endswith('.jpg'):
+        print('image/jpg')
+    elif filename.endswith('.jpeg'):
+        print('image/jpeg')
+    elif filename.endswith('.png'):
+        print('image/png')
+    elif filename.endswith('.pdf'):
+        print('image/pdf')
+    elif filename.endswith('.txt'):
+        print('image/txt')
+    elif filename.endswith('.zip'):
+        print('image/zip')
+    else:
+        print('application/octet-stream')
 
 main()
