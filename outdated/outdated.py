4be24cba2acdbf2442d825ed9c3334be94912089
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-02 15:02:54 +0000

    Wed, Aug 2, 2023, 8:02 PM +05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,6 +1,24 @@
 def main():
-
-
+    months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    ]
+    while True:
+        try:
+            date = input('Date: ')
+            break
+        except ValueError:
+            pass
 
 
 main()
