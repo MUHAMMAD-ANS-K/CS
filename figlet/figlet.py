b8fac09b466a4e16b5b2b731ff7ae04f62b20012
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-02 22:12:30 +0000

    Thu, Aug 3, 2023, 3:12 AM +05:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -7,6 +7,8 @@ font.getFonts()
 if len(sys.argv) == 1:
     font.setFont(font = 'slant')
 elif len(sys.argv) == 3:
+    if sys.argv[1] != '-f':
+        sys.exit('Invalid usage')
     font.setFont(font = sys.argv[2])
 else:
     sys.exit('Invalid usage')
