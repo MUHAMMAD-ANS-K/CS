d10b89331837ae64f981e0004a32bcde6fa3a32c
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-02 15:23:26 +0000

    Wed, Aug 2, 2023, 8:23 PM +05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,10 +19,12 @@ def main():
             for i in range(len(months)):
                 if months[i] == month:
                     month = i + 1
+            month = int(month)
+            day = int(day)
             if month <= 12 and day <= 31:
                 break
         except ValueError:
             pass
-    print(f'{year}-{month}-{day}')
+    print(f'{year}-{month:.1f}-{day}')
 
 main()
