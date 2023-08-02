7eaf6ec218d54106983019a911220e7cdebc9e40
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-02 15:13:11 +0000

    Wed, Aug 2, 2023, 8:13 PM +05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -16,10 +16,13 @@ def main():
     while True:
         try:
             month,day,year = input('Date: ').split('/')
+            for i in range(len(months)):
+                if months[i] == month:
+                    month = i
             if int(month) <= 12 and int(day) <= 31:
                 break
         except ValueError:
             pass
-
+    print(f'{month} {day} {year}')
 
 main()
