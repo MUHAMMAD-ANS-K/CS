55defd9740441a746a6b8d0949e5b802efe35345
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-02 19:04:44 +0000

    Thu, Aug 3, 2023, 12:04 AM +05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,15 +19,15 @@ def main():
             month,day,year = date.split('/')
             break
         except ValueError:
-            try:
+           # try:
                 month,day,year = date.split(' ')
                 day = day.rstrip(',')
                 for i in range(len(months)):
                     if month == months:
                         month = i + 1
                         break
-            except ValueError:
-                pass
+          #  except ValueError:
+           #     pass
     month = int(month)
     day = int(day)
     if month <= 12 and day <= 31:
