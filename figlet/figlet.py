5b662df1e62a00f00d5ae4b9934165555ea2238d
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-02 21:56:14 +0000

    Thu, Aug 3, 2023, 2:56 AM +05:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,11 +1,11 @@
 from pyfiglet import Figlet
 import sys
 
-font = font.getFonts
+font = font.getFonts()
 if len(sys.argv) == 1:
     font = Figlet(font = 'slant')
 elif len(sys.argv) == 3:
-    font = Figlet(font = argv[1] + argv[2])
+    font = Figlet(font = sys.argv[1] + sys.argv[2])
 else:
     sys.exit('Invalid usage')
 
