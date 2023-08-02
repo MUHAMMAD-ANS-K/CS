41038109b0ff2f54dafdde33d1e30b4b37a4436d
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-02 14:37:56 +0000

    Wed, Aug 2, 2023, 7:37 PM +05:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -15,7 +15,7 @@ def main():
         try:
          item = input('Item: ').lower().title()
          total += item_price[item]
-         print(f'Total: ${total: .2f}')
+         print(f'Total: ${total:.2f}')
         except KeyError:
            pass
         except EOFError:
