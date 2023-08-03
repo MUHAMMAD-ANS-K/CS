f73753272486b6cb0f2bf5a064b80e00c93d5822
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 13:27:04 +0000

    Thu, Aug 3, 2023, 6:27 PM +05:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,10 +1,17 @@
+import sys
 def main():
+    level = get_level()
+
+
+
+def get_level():
     while True:
         try:
             n = int(input('Level: ').strip())
             if n == 1 or n == 2 or n == 3:
-                break
+                sys.exit()
         except ValueError:
             pass
 
-main()
+if __name__ == '__main__':
+    main()
