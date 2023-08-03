0c933e287b8f934a0a1e2650e0923fa3004bf52c
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 19:47:00 +0000

    Fri, Aug 4, 2023, 12:47 AM +05:00

--- twttr/test_twttr/test_twttr.py
+++ twttr/test_twttr/test_twttr.py
@@ -1,6 +1,9 @@
 from twttr import shorten
 
-def main():
+def test_cvowels():
     assert shorten('twitter') == 'twttr'
-    ass
+    assert shorten('aeiou') == ''
 
+def test_wvowels():
+    assert shorten('twttr') == 'twttr'
+    assert shorten('#$@1') == '#$@1'
