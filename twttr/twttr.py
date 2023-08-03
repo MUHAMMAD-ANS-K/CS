e221d6cd010a529ece157b4ddc6876d73bd94a49
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 19:36:12 +0000

    Fri, Aug 4, 2023, 12:36 AM +05:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,24 +1,19 @@
 def main():
-
-
-    print()
-main()
-
-def main():
     in_put = input('Input: ')
+    print(short)
 
 
 def shorten(word):
-        out = word.lower()
-        j = 0
+    out = word.lower()
+    j = 0
     for i in out:
         match i:
             case 'a' | 'e'| 'i' | 'o' | 'u':
-                None
+                pass
             case _ :
-                print(word[j],end = '')
+                short += word[j]
         j += 1
-
+    return short
 
 if __name__ == "__main__":
     main()
