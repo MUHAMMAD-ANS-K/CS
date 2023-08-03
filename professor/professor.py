52550e7577f7cbc0ecb8e84226f1bab10e12c382
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 13:14:09 +0000

    Thu, Aug 3, 2023, 6:14 PM +05:00

--- professor/professor.py
+++ professor/professor.py
@@ -26,7 +26,7 @@ def main():
 def get_level(prompt):
     while True:
         try:
-            n = int(input(prompt))
+            n = int(input(prompt).strip())
             if n == 1 or n == 2 or n == 3:
                 return n
         except ValueError:
