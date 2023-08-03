edd4be4fa722926fcd7b04666721bbf75857f410
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 13:20:51 +0000

    Thu, Aug 3, 2023, 6:20 PM +05:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,26 +1,9 @@
-import random
-
 def main():
-    level = get_level('Level:')
-    score = 0
-    for _ in range(10):
-        x = random.randint(1,9) * 10 ** (level - 1)
-        y = random.randint(1,9) * 10 ** (level - 1)
-        total = x + y
-        for i in range(3):
-            print(f'{x} + {y} = ', end = '')
-            try:
-                user_input = int(input())
-            except ValueError:
-                print('EEE')
-            if user_input != total:
-                print('EEE')
-            elif user_input == total:
-                score += 1
-                break
-        if user_input != total:
-            print(total)
-    print(score)
+    level = get_level('Level: ')
+
+
+
+
 
 
 def get_level(prompt):
@@ -31,6 +14,5 @@ def get_level(prompt):
                 return n
         except ValueError:
             pass
-
 
 main()
