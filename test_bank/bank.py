1bedc39690027f3395e1afefcad043b85beef808
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 20:26:49 +0000

    Fri, Aug 4, 2023, 1:26 AM +05:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,14 +1,14 @@
 def main():
     greet = input("Greeting: ")
-    print('$' + value(greet))
+    print('$' + str(value(greet)))
 def value(greeting):
     greeting = greeting.lower().strip()
     if greeting.startswith('hello'):
-        return '0'
+        return 0
     elif greeting.startswith('h'):
-        return '20'
+        return 20
     else:
-        return '100'
+        return 100
 
 if __name__ == '__main__':
     main()
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,12 +1,12 @@
 from bank import value
 
 def test_hello():
-    assert value('hello') == '0'
-    assert value('HELLO') == '0'
-    assert value('Hello') == '0'
+    assert value('hello') == 0
+    assert value('HELLO') == 0
+    assert value('Hello') == 0
 def test_h():
-    assert value('How are you?') == '20'
-    assert value('hi') == '20'
+    assert value('How are you?') == 20
+    assert value('hi') == 20
 def test_wouth():
-    assert value('Greetings') == '100'
-    assert value('brosky') == '100'
+    assert value('Greetings') == 100
+    assert value('brosky') == 100
