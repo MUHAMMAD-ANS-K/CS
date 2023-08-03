5de335dad9ddd4cd0b6de920931078dfcf930b9c
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 20:58:02 +0000

    Fri, Aug 4, 2023, 1:58 AM +05:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -10,7 +10,7 @@ def test_12alpha():
     assert is_valid('Ho2') == True
     assert is_valid('2HO') == False
     assert is_valid('02') == False
-    assert is_valid('H2ki') == False
+    assert is_valid('HK') == True
 
 def test_numbers():
     assert is_valid('CS50') == True
