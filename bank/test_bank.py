f238acf417afdd8a5252510b124271c38c093252
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 20:21:56 +0000

    Fri, Aug 4, 2023, 1:21 AM +05:00

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -4,3 +4,9 @@ def test_hello():
     assert value('hello') == '0'
     assert value('HELLO') == '0'
     assert value('Hello') == '0'
+def test_h():
+    assert value('How are you?') == '20'
+    assert value('hi') == '20'
+def test_wouth():
+    assert value('Greetings') == '100'
+    assert value('brosky') == '100'
