d16116df7b0eab19bcf18af2589696697463dc06
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-03 21:28:36 +0000

    Fri, Aug 4, 2023, 2:28 AM +05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -5,18 +5,17 @@ def main():
 
 def convert(fract):
     while True:
-        try:
-            x,y = fract.split('/')
-            x = int(x)
-            y = int(y)
-            if x <= y:
-                return round((x / y) * 100)
-        except ValueError:
-            fract = input('Fraction: ')
-            pass
-        except ZeroDivisionError:
-            fract = input('Fraction: ')
-            pass
+        x,y = fract.split('/')
+        x = int(x)
+        y = int(y)
+        if x <= y:
+            return round((x / y) * 100)
+        #except ValueError:
+         #   fract = input('Fraction: ')
+              #pass
+        #except ZeroDivisionError:
+         #   fract = input('Fraction: ')
+          #  pass
 
 def gauge(percentage):
     if percentage <= 1:
