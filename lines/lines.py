1c8ddbc2ef7be41e77784f68424d8e7a9c841054
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 18:44:23 +0000

    Fri, Aug 4, 2023, 11:44 PM +05:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,8 +1,9 @@
 import sys
 def main():
-    file_name = argv[1]
+    
+    file_name = sys.argv[1]
     with open(file_name) as file:
-        lines = file.readlines()
-
+        for line in file:
+            print(line.rstrip())
 if __name__ == '__main__':
     main()
