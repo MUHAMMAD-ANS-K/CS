de39848887d9b9a19d56b65a30c3072584030f6e
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 19:57:05 +0000

    Sat, Aug 5, 2023, 12:57 AM +05:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -12,6 +12,7 @@ def main():
             sys.exit('Not a CSV file')
         with open(file_name) as file:
             reader = csv.reader(file)
+            #for line in reader:
             print(tabulate(reader, tablefmt="outline"))
 
     except FileNotFoundError:
