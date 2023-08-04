ee25c021d96340aab2888e20ff1996c6a72be063
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 18:49:41 +0000

    Fri, Aug 4, 2023, 11:49 PM +05:00

--- lines/lines.py
+++ lines/lines.py
@@ -4,7 +4,7 @@ def main():
     file_name = sys.argv[1]
     with open(file_name) as file:
         for line in file:
-            line = line.lstrip()
+            #line = line.lstrip()
             if line.startswith(' '):
                 pass
             else:
