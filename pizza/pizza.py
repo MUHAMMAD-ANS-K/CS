28e92ed30568ace2a0cb259b676297b1916511d1
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 20:12:56 +0000

    Sat, Aug 5, 2023, 1:12 AM +05:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,21 +1,24 @@
 import sys
 import csv
 from tabulate import tabulate
+
+
 def main():
     if len(sys.argv) > 2:
-        sys.exit('Too many command-line arguments')
+        sys.exit("Too many command-line arguments")
     elif len(sys.argv) == 1:
-        sys.exit('Too few command-line arguments')
+        sys.exit("Too few command-line arguments")
     try:
         file_name = sys.argv[1]
-        if not file_name.endswith('.csv'):
-            sys.exit('Not a CSV file')
+        if not file_name.endswith(".csv"):
+            sys.exit("Not a CSV file")
         with open(file_name) as file:
             reader = csv.reader(file)
-            print(tabulate(reader,headers = 'firstrow', tablefmt="grid"))
+            print(tabulate(reader, headers="firstrow", tablefmt="grid"))
 
     except FileNotFoundError:
-        sys.exit('File does not exist')
+        sys.exit("File does not exist")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
