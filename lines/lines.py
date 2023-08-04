f274e7ef678f032c0a0e3fa52e2b0635d2e9ba54
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 19:16:22 +0000

    Sat, Aug 5, 2023, 12:16 AM +05:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,25 +1,29 @@
 import sys
+
+
 def main():
     if len(sys.argv) > 2:
-        sys.exit('Too many command-line arguments')
+        sys.exit("Too many command-line arguments")
     lines = 0
     try:
         file_name = sys.argv[1]
-        if not file_name.endswith('.py'):
-            sys.exit('Not a Python file')
+        if not file_name.endswith(".py"):
+            sys.exit("Not a Python file")
         with open(file_name) as file:
             for line in file:
                 if line.isspace():
                     pass
-                elif line.lstrip().startswith('#'):
+                elif line.lstrip().startswith("#"):
                     pass
                 else:
                     lines += 1
     except FileNotFoundError:
-        sys.exit('File does not exist')
+        sys.exit("File does not exist")
     except IndexError:
-        sys.exit('Too few command-line arguments')
+        sys.exit("Too few command-line arguments")
 
     print(lines)
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
