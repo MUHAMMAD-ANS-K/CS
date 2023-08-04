de308deab8aa1a2add541d7addd3830587a03052
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 18:57:49 +0000

    Fri, Aug 4, 2023, 11:57 PM +05:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,14 +1,19 @@
 import sys
 def main():
     lines = 0
-    #file_name = sys.argv[1]
-    with open('figlet.py') as file:
-        for line in file:
-            #line = line.lstrip()
-            if line.startswith(' '):
-                pass
-            else:
-                lines += 1
+    try:
+        file_name = sys.argv[1]
+        with open(file_name) as file:
+            for line in file:
+                line = line.lstrip()
+                if line.startswith('#'):
+                    pass
+                else:
+                    lines += 1
+    except FileNotFoundError:
+        pass
+    except IndexError:
+        sys.exit('T')
 
     print(lines)
 if __name__ == '__main__':
