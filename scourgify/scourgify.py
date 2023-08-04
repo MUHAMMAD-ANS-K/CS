85e72d8593f2679983885bad552109bc7c6939ec
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 20:17:37 +0000

    Sat, Aug 5, 2023, 1:17 AM +05:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,7 +5,10 @@ def main():
     elif len(sys.argv) == 1:
         sys.exit("Too few command-line arguments")
     try:
-        
+
+    except FileNotFoundError:
+        sys.exit('Could not read invalid_file.csv')
+
 
 if __name__ == '__main__':
     main()
