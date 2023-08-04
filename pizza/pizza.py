7b48eec269418a09d5934f036748b7f761959ca7
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 19:36:49 +0000

    Sat, Aug 5, 2023, 12:36 AM +05:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -12,6 +12,7 @@ def main():
             sys.exit('Not a CSV file')
         with open(file_name) as file:
             table = csv.reader(file)
+            print(table)
 
     except FileNotFoundError:
         sys.exit('File does not exist')
