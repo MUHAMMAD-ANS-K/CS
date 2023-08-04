341d27ada0c10ef88084fdd41bb1cff4190423a0
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-04 19:15:39 +0000

    Sat, Aug 5, 2023, 12:15 AM +05:00

--- lines/lines.py
+++ lines/lines.py
@@ -5,12 +5,13 @@ def main():
     lines = 0
     try:
         file_name = sys.argv[1]
-        #if not file_name.endswith('.py'):
-         #   sys.exit('Not a Python file')
+        if not file_name.endswith('.py'):
+            sys.exit('Not a Python file')
         with open(file_name) as file:
             for line in file:
-                #line = line.lstrip()
                 if line.isspace():
+                    pass
+                elif line.lstrip().startswith('#'):
                     pass
                 else:
                     lines += 1
