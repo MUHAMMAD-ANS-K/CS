fbe3752cf9c37508618f0d11784de862ce3e2884
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-06 18:53:20 +0000

    Sun, Aug 6, 2023, 11:53 PM +05:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,8 +5,8 @@ def main():
 
 def test_1():
     assert validate(r'127.0.0.1') == 'True'
-    assert validate(r'255.255.255.255') == 'True'
-    assert validate(r'140.247.235.144') == 'True'
+    assert validate(r'255.255') == 'False'
+    assert validate(r'140') == 'False'
     assert validate(r'256.255.255.255') == 'False'
 
 def test_2():
