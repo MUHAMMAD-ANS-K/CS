32b3227c677336cef010a7dacf9dec1b4ce8053a
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-06 14:19:19 +0000

    Sun, Aug 6, 2023, 7:19 PM +05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -6,7 +6,7 @@ def main():
 
 
 def validate(ip):
-    if re.search(r"^[0-2][0-5][0-9]\.[0-255]{1,3}\.[0-255]{1,3}\.[0-255]{1,3}$", ip):
+    if re.search(r"^[0-2]?[0-9]?[0-9]?\.[0-255]{1,3}\.[0-255]{1,3}\.[0-255]{1,3}$", ip):
         return "True"
     else:
         return "False"
