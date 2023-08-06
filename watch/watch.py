33d6c793ad2dd37f61548c019141b704391acfbc
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-06 19:45:17 +0000

    Mon, Aug 7, 2023, 12:45 AM +05:00

--- watch/watch.py
+++ watch/watch.py
@@ -7,14 +7,11 @@ def main():
 
 
 def parse(s):
-    match = re.search(r'<iframe(.+)? src="https?://(www\.)?youtube.com/embed(/\w+)"(.+)?></iframe>',s)
+    match = re.search(r'<iframe(.+)? src="https?://(www\.)?youtube.com/embed(/[0-9a-zA-Z_-]+)"(.+)?></iframe>',s)
     if match:
         return 'https://youtu.be' + match.group(3)
     else:
         return None
-
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/OmJ-4B-mS-Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 
 if __name__ == "__main__":
