b512e70b51d7c4adafe69c3f8c91d6dd5d8012cb
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-06 15:24:39 +0000

    Sun, Aug 6, 2023, 8:24 PM +05:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,3 +5,10 @@ def test_true():
     assert validate('255.0.255.0') == 'True'
     assert validate('1.1.1.1') == 'True'
     assert validate('99.97.156.254') == 'True'
+
+def test_false():
+    assert validate('cat') == 'False'
+    assert validate('256.45.23.0') == 'False'
+    assert validate('222.3.2.cat') == 'False'
+    assert validate('#.99.8.44') == 'False'
+    assert validate('233.32.2322') == 'False'
