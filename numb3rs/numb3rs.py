827b4c7a18797813a5f744267dc5ce526593f925
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-06 18:55:16 +0000

    Sun, Aug 6, 2023, 11:55 PM +05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,9 +7,9 @@ def main():
 
 def validate(ip):
     if re.search(r"^(([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])\.){3}([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])$", ip):
-        return "True"
+        return True
     else:
-        return "False"
+        return False
 
 
 if __name__ == "__main__":
--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -4,17 +4,17 @@ def main():
     test_2()
 
 def test_1():
-    assert validate(r'127.0.0.1') == 'True'
-    assert validate(r'255.255') == 'False'
-    assert validate(r'140') == 'False'
-    assert validate(r'256.255.255.255') == 'False'
+    assert validate(r'127.0.0.1') == True
+    assert validate(r'255.255') == False
+    assert validate(r'140') == False
+    assert validate(r'256.255.255.255') == False
 
 def test_2():
-    assert validate(r'cat') == 'False'
-    assert validate(r'8.8.8') == 'False'
-    assert validate(r'10.10.10.10.10') == 'False'
-    assert validate(r'64.128.256.512') == 'False'
-    assert validate(r'dog') == 'False'
+    assert validate(r'cat') == False
+    assert validate(r'8.8.8') == False
+    assert validate(r'10.10.10.10.10') == False
+    assert validate(r'64.128.256.512') == False
+    assert validate(r'dog') == False
 
 if __name__ == '__main__':
     main()
