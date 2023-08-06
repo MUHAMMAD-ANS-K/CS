3912e010a3f70efdf7041bf120d505db154ae32b
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-06 18:52:11 +0000

    Sun, Aug 6, 2023, 11:52 PM +05:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -4,17 +4,17 @@ def main():
     test_2()
 
 def test_1():
-    assert validate('127.0.0.1') == 'True'
-    assert validate('255.255.255.255') == 'True'
-    assert validate('140.247.235.144') == 'True'
-    assert validate('256.255.255.255') == 'False'
+    assert validate(r'127.0.0.1') == 'True'
+    assert validate(r'255.255.255.255') == 'True'
+    assert validate(r'140.247.235.144') == 'True'
+    assert validate(r'256.255.255.255') == 'False'
 
 def test_2():
-    assert validate('cat') == 'False'
-    assert validate('8.8.8') == 'False'
-    assert validate('10.10.10.10.10') == 'False'
-    assert validate('64.128.256.512') == 'False'
-    assert validate('dog') == 'False'
+    assert validate(r'cat') == 'False'
+    assert validate(r'8.8.8') == 'False'
+    assert validate(r'10.10.10.10.10') == 'False'
+    assert validate(r'64.128.256.512') == 'False'
+    assert validate(r'dog') == 'False'
 
 if __name__ == '__main__':
     main()
