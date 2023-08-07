7b7078604e10334382918daa14208cdd65f0661e
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-07 10:59:34 +0000

    Mon, Aug 7, 2023, 3:59 PM +05:00

--- working/test_working.py
+++ working/test_working.py
@@ -4,4 +4,4 @@ def test_1():
     assert convert('9 AM to 9 AM') == '09:00 to 09:00'
     assert convert('10 PM to 9 AM') == '22:00 to 09:00'
     assert convert('12:39 AM to 12:42 PM') == '00:39 to 12:42'
-    assert convert('9AM to 10PM') ==
+    with pytest.raises(ValueError)
