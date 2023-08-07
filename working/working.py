5425618634baf6a1ff2488b384982ae2dd465fd4
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-07 10:41:15 +0000

    Mon, Aug 7, 2023, 3:41 PM +05:00

--- working/working.py
+++ working/working.py
@@ -7,21 +7,24 @@ def main():
 
 
 def convert(s):
-    start, end = s.strip().split(' to ')
-    time1 = re.search(r'^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$',start)
-    time2 = re.search(r'^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$',end)
-    if not(time1 and time2):
+    start, end = s.strip().split(" to ")
+    time1 = re.search(r"^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$", start)
+    time2 = re.search(r"^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$", end)
+    if not (time1 and time2):
         raise ValueError()
-    time_24a = int(time1.group(1))
-    time_24b = int(time2.group(1))
-    if time1.group(3) == 'P':
-        time_24a += 12
-    if time2.group(3) == 'P':
-        time_24b += 12
-    return f'{time_24a:02d}{time1.group(2)} to {time_24b}'
-
-
-...
+    hours_24s = int(time1.group(1))
+    hours_24e = int(time2.group(1))
+    minutes_s = time1.group(2)
+    minutes_e = time2.group(2)
+    if minutes_s == None:
+        minutes_s = ":00"
+    if minutes_e == None:
+        minutes_e = ":00"
+    if time1.group(3) == "P":
+        time_24as += 12
+    if time2.group(3) == "P":
+        time_24e += 12
+    return f"{hours_24s:02d}{minutes_s} to {hours_24e}{minutes_e}"
 
 
 if __name__ == "__main__":
