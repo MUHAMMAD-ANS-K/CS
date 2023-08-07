3b656ceb6cba61356e14a4ebc6846323cb9ec0ce
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-07 10:19:36 +0000

    Mon, Aug 7, 2023, 3:19 PM +05:00

--- working/working.py
+++ working/working.py
@@ -11,8 +11,9 @@ def convert(s):
         start, end = s.strip().split(' to ')
         time1 = re.search(r'^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$',start)
         time2 = re.search(r'^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$',end)
-        if time1 and time2:
-            return raise ValueError('not enough values to unpack')
+        if not(time1 and time2):
+            sys.exception()
+            #return False
     except ValueError:
         pass
 
