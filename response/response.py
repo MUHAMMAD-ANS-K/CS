34cced653348fa9e73f0b6d76c6bedd2d78178b9
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-07 14:05:32 +0000

    Mon, Aug 7, 2023, 7:05 PM +05:00

--- response/response.py
+++ response/response.py
@@ -3,7 +3,7 @@ from validator_collection import checkers
 def main():
     print(valid(input('Email: ')))
 
-def valid(email)
+def valid(email):
     email_true = checkers.is_email(email)
     if email_true:
         return 'Valid'
