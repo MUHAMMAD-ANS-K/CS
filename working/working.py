cac0dbc126607c3084d2dc2083c9786630265e6b
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-07 10:28:07 +0000

    Mon, Aug 7, 2023, 3:28 PM +05:00

--- working/working.py
+++ working/working.py
@@ -12,7 +12,11 @@ def convert(s):
     time2 = re.search(r'^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$',end)
     if not(time1 and time2):
         raise ValueError()
-
+    if time1.group(3) == 'P':
+        time1.group(1) = int(time1.group(1)) + 12
+    if time2.group(3) == 'P':
+        time2.group(1) = int(time2.group(1)) + 12
+    return f'{time1.group(1)} to {time2.group(1)}'
 
 
 ...
