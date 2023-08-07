c8019b31021bec44c245777ba451c143c9300c4a
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-07 13:50:19 +0000

    Mon, Aug 7, 2023, 6:50 PM +05:00

--- um/test_um.py
+++ um/test_um.py
@@ -1,4 +1,8 @@
 from um import count
 
 def test_1():
-    assert
+    assert count('um umu um') == 2
+    assert count('um.?') == 1
+    assert count('yummy') == 0
+    assert count('0um3') == 0
+    assert count('UM, I am sorry,um') == 2
