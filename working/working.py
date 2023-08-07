14c68a4618e89aa3023bc497e83aae577e59f66f
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-07 10:05:17 +0000

    Mon, Aug 7, 2023, 3:05 PM +05:00

--- working/working.py
+++ working/working.py
@@ -7,9 +7,10 @@ def main():
 
 
 def convert(s):
-    #start, end = s.split(' to ')
-    s = re.search(r'^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$',s)
-    if s:
+    start, end = s.strip().split(' to ')
+    time1 = re.search(r'^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$',start)
+    time2 = re.search(r'^(1[0-2]|[0-9])(:[0-5][0-9])? ([AP])M$',end)
+    if time2:
         return True
     else:
         return False
