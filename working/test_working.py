f0e0ece10d1b10449daee6dbc06e6b5395c61dd3
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-07 11:03:28 +0000

    Mon, Aug 7, 2023, 4:03 PM +05:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,7 +1,14 @@
 from working import convert
-
+import pytest
 def test_1():
     assert convert('9 AM to 9 AM') == '09:00 to 09:00'
     assert convert('10 PM to 9 AM') == '22:00 to 09:00'
     assert convert('12:39 AM to 12:42 PM') == '00:39 to 12:42'
-    with pytest.raises(ValueError)
+
+def test_2():
+    with pytest.raises(ValueError):
+        convert('9AM to 10PM')
+    with pytest.raises(ValueError):
+        convert('10 AM to 12:60 AM')
+    with pytest.raises(ValueError):
+        convert('9:30 am 10:40 pm')
