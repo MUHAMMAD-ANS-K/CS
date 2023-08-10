2b94ade6ee7e04ca869e36eb0d0875ec8e34450d
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 15:04:41 +0000

    Thu, Aug 10, 2023, 8:04 PM +05:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -9,7 +9,7 @@ for i in sentence:
         words += 1
     if i in ['!','.','?']:
         sentences += 1
-L = round((letters/words)) * 100
+L = round((letters/words),2) * 100
 S = round((sentences/words)) * 100
 index = 0.0588 * L - 0.296 * S - 15.8
-print(index)
+print(L)
