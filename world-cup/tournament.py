81f8c546c45071f2aefd0a30237828b731985be6
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 11:18:55 +0000

    Thu, Aug 10, 2023, 4:18 PM +05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -35,9 +35,8 @@ def main():
         else:
             counts[winner] += 1
     # Print each team's chances of winning, according to simulation
-    for team in sorted(counts, key=lambda team: counts[team],reverse=True):
-        print(team)
-        #print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
+    for team in sorted(counts, key=lambda team: counts[team], reverse=True):
+        print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
 
 def simulate_game(team1, team2):
