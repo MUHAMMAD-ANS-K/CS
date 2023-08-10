220a1360245bc923f2f65402fc7f9b63cf1ff9d4
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 12:02:23 +0000

    Thu, Aug 10, 2023, 5:02 PM +05:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -5,5 +5,7 @@ while True:
             break
     except ValueError:
         pass
+# A for loop in range from 1 to height.
 for i in range(1, height + 1):
+    # Printing all the stuff by setting sep parameter to empty.
     print(" " * (height - i), "#" * i, "  ", "#" * i, sep="")
