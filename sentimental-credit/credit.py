623e7af4f2622a12ecb056e1a39e7ee3b8e0b4df
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 12:47:00 +0000

    Thu, Aug 10, 2023, 5:47 PM +05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,7 +4,7 @@ number = input('Number: ')
 numbers = list()
 for i in number:
     numbers.append(int(i))
-for j in range(-2,len(numbers,2)):
+for j in range(-2,len(numbers),2):
     print(numbers[j])
 
 
