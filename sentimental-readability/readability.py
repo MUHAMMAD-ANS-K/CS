1198d462d4b2150b8a3eefbccc7a05c97a202ad8
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 14:44:57 +0000

    Thu, Aug 10, 2023, 7:44 PM +05:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,6 +5,9 @@ sentences = 0
 for i in sentence:
     if i.isalpha():
         letters += 1
+    if i.isspace():
+        words += 1
+    if i in ['!','.','?']:
+        sentences += 1
 
-
-print(letters)
+print(letters,words,sentences)
