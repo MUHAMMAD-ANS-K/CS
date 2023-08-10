6db4dde79f02d37582d65d0b4ec02393625b0728
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 13:10:44 +0000

    Thu, Aug 10, 2023, 6:10 PM +05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,23 +1,16 @@
-
-
-number = input('Number: ')
+number = input("Number: ")
 numbers = list()
 for i in number:
     numbers.append(int(i))
 sum = 0
 range_stop = -len(numbers) - 1
-for j in range(-2,range_stop,-2):
+for j in range(-2, range_stop, -2):
     num = numbers[j] * 2
     if num > 9:
-       sum += int((num / 10)) + (num % 10)
+        sum += int((num / 10)) + (num % 10)
     else:
         sum += num
-for k in range(-1,range_stop,-2):
+for k in range(-1, range_stop, -2):
     sum += numbers[k]
 
 print(sum)
-
-
-
-
-
