d99dacd70fdaefd35d8d99d17f1fa9b0de79116c
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 15:12:55 +0000

    Thu, Aug 10, 2023, 8:12 PM +05:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -7,9 +7,14 @@ for i in sentence:
         letters += 1
     if i.isspace():
         words += 1
-    if i in ['!','.','?']:
+    if i in ["!", ".", "?"]:
         sentences += 1
-L = (letters/words) * 100
-S = (sentences/words) * 100
+L = (letters / words) * 100
+S = (sentences / words) * 100
 index = round(0.0588 * L - 0.296 * S - 15.8)
-print(f"Grade {index}")
+if index < 1:
+    print("Before Grade 1")
+elif index >= 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {index}")
