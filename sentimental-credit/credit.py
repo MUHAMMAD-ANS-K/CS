c2a60fa2c19237b83ab2384198281f4321b15959
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 12:38:45 +0000

    Thu, Aug 10, 2023, 5:38 PM +05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,9 +1,8 @@
 import re
 
-number = int(input('Number: '))
+number = input('Number: ')
 numbers = list()
-for _ in range(len(number)):
-    numbers.append(number % 10)
-    number = number / 10
+for i in number:
+    print(i)
 
-print(numbers)
+
