2f0303b15fcdc957c0c6e00726d982579ca68ff0
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 13:25:49 +0000

    Thu, Aug 10, 2023, 6:25 PM +05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,23 +1,37 @@
-number = input("Number: ")
-numbers = list()
-for i in number:
-    numbers.append(int(i))
-sum = 0
-range_stop = -len(numbers) - 1
-for j in range(-2, range_stop, -2):
-    num = numbers[j] * 2
-    if num > 9:
-        sum += int((num / 10)) + (num % 10)
+def main():
+    number = input("Number: ")
+    if card(number) == "INVALID":
+        print("INVALID")
+    numbers = list()
+    for i in number:
+        numbers.append(int(i))
+    sum = 0
+    range_stop = -len(numbers) - 1
+    for j in range(-2, range_stop, -2):
+        num = numbers[j] * 2
+        if num > 9:
+            sum += int((num / 10)) + (num % 10)
+        else:
+            sum += num
+    for k in range(-1, range_stop, -2):
+        sum += numbers[k]
+
+    if sum % 10 == 0:
+        print(card(number))
     else:
-        sum += num
-for k in range(-1, range_stop, -2):
-    sum += numbers[k]
+        print("INVALID")
 
 
+def card(number):
+    if number.startswith(("34", "37")) and len(number) == 15:
+        return "AMEX"
+    if number.startswith(("51", "52", "53", "54", "55")) and len(number) == 16:
+        return "MASTERCARD"
+    if number.startswith("4") and (len(number) == 13 or len(number) == 16):
+        return "VISA"
+    else:
+        return "INVALID"
 
-print(sum)
 
-
-
-def card(list_num):
-    if 
+if __name__ == "__main__":
+    main()
---
+++ sentimental-credit/hi.py
@@ -0,0 +1,3 @@
+inpu = input()
+if inpu.startswith(('1','2','3','4')):
+    print('hi')
