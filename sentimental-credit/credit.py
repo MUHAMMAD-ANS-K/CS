82ed49a8cbb019346de3dad9e12bc0767d2fefd1
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 13:00:36 +0000

    Thu, Aug 10, 2023, 6:00 PM +05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,7 +7,7 @@ for i in number:
 sum = 0
 for j in range(-2,-len(numbers) - 1,-2):
     num = numbers[j] * 2
-    sum += (num / 10) + (num % 10) if num > 9 else
+    sum += (num / 10) + (num % 10) if num > 9 else sum + num
 print(sum)
 
 
