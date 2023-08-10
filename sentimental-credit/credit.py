1add9485406985043471f8026016332963a505ad
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 12:59:26 +0000

    Thu, Aug 10, 2023, 5:59 PM +05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,11 +1,16 @@
-import re
+
 
 number = input('Number: ')
 numbers = list()
 for i in number:
     numbers.append(int(i))
+sum = 0
 for j in range(-2,-len(numbers) - 1,-2):
-    print(numbers[j],end = '')
+    num = numbers[j] * 2
+    sum += (num / 10) + (num % 10) if num > 9 else
+print(sum)
+
+
 
 
 
