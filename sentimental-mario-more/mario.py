c4e18d7fc78c9a67b6d8685f7fe199edd2483958
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-10 12:00:35 +0000

    Thu, Aug 10, 2023, 5:00 PM +05:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,9 +1,9 @@
 while True:
     try:
-        height = int(input('Height: '))
+        height = int(input("Height: "))
         if 9 > height > 0:
             break
     except ValueError:
         pass
-for i in range(1,height + 1):
-    print(" " * (height - i), '#' * i,sep = '')
+for i in range(1, height + 1):
+    print(" " * (height - i), "#" * i, "  ", "#" * i, sep="")
