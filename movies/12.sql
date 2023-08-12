f02ef34089944236d607dd1331437413c7efa9fb
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-12 11:37:24 +0000

    Sat, Aug 12, 2023, 4:37 PM +05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id WHERE name = 'Jennifer Lawrence', 'Bradley Cooper';
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id WHERE name = 'Bradley Cooper' IN (SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id WHERE name = 'Jennifer Lawrence');
