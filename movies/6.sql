9e8e0f674e204bc91959ff4c32b540b9885c57c8
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-12 10:41:35 +0000

    Sat, Aug 12, 2023, 3:41 PM +05:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,1 +1,1 @@
-SELECT AVG(rating) FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = '2012' LIMIT 5;
+SELECT AVG(rating) FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = '2012';
