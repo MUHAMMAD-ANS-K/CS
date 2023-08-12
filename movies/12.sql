076d24c9f24a7abdb339051085c74b2d310186b8
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-12 11:42:21 +0000

    Sat, Aug 12, 2023, 4:42 PM +05:00

--- movies/12.sql
+++ movies/12.sql
@@ -5,7 +5,7 @@ FROM
   JOIN stars ON movies.id = stars.movie_id
   JOIN people ON people.id = stars.person_id
 WHERE
-  name = 'Bradley Cooper' IN (
+  name = 'Bradley Cooper' AND title IN (
     SELECT
       title
     FROM
