d704ac15e9faa73abca09284c42bee2d8ee3e887
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-12 10:48:31 +0000

    Sat, Aug 12, 2023, 3:48 PM +05:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE rating > '9.0' ORDER BY rating;
+SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = '2010' ORDER BY rating DESC, title;
