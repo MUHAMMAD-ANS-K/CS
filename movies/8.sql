cede2230a9a8ed2484afe86c6cb461a1c1adde6f
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-12 10:55:12 +0000

    Sat, Aug 12, 2023, 3:55 PM +05:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,1 +1,1 @@
-SELECT title, name FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id WHERE title = 'Toy Story';
+SELECT name FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id WHERE title = 'Toy Story';
