712fae8a229d9c300833eae3493bfa79cc99eefd
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-08-12 11:51:35 +0000

    Sat, Aug 12, 2023, 4:51 PM +05:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,7 +1,9 @@
 SELECT
-    name
+    title
 FROM
     movies
     JOIN stars ON stars.movie_id = movies.id
     JOIN people ON stars.person_id = people.id
 WHERE
+    name = 'Kevin Bacon'
+    AND birth = 1958;
