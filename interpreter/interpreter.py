73cac5a424ca5a13bb1539764cb27a61118aae09
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-09-27 17:31:41 +0000

    Wed, Sep 27, 2023, 10:31 AM -07:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,5 +1,13 @@
 def main():
     user_input = input("Expression: ")
-
+    x, y, z = user_input().split(" ")
+    if y == "+":
+        print(float(x + y))
+    elif y == "-":
+        print(float(x - y))
+    elif y == "/":
+        print(float(x / y))
+    else:
+        print(float(x * y))
 if __name__ == "__main__":
     main()
