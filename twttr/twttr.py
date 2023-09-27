ff98611180efd15bb1be864e5b223dd7b913d407
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-09-27 18:12:21 +0000

    Wed, Sep 27, 2023, 11:12 AM -07:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,11 @@
 def main():
     user_input = input("Input: ").strip().lower()
     for chr in user_input:
-        match(chr)
+        match(chr):
+            case 'a' | 'e' | 'i' | 'o' | 'u':
+                pass
+            case _:
+                print(chr,end='')
+    print()
 if __name__ == "__main__":
     main()
