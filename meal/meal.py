6c1cdfe5c69190246067405cd8b530919f9ed769
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-09-27 17:36:27 +0000

    Wed, Sep 27, 2023, 10:36 AM -07:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,18 +1,9 @@
 def main():
-    t = input('What time is it?')
-    t = convert(t)
-    if 7.0 <= t <= 8.0:
-        print('breakfast time')
-    elif 12.0 <= t <= 13.0:
-        print('lunch time')
-    elif 18.0 <= t <= 19.0:
-        print('dinner time')
+    ...
+
 
 def convert(time):
-    hours, minutes = time.split(':')
-    minutes = float(minutes) / 60
-    hours = round(float(hours) + minutes, 2)
-    return hours
+    ...
 
 
 if __name__ == "__main__":
