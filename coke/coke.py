61ddecb7228a1a841733ef73cc88b566caceb489
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-09-27 18:04:55 +0000

    Wed, Sep 27, 2023, 11:04 AM -07:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,4 +1,9 @@
 def main():
-
+    cents = 50
+    while cents > 0:
+        print(f"Amount Due: {cents}")
+        coin = int(input("Insert Coin: ").strip())
+        if coin == 5 or coin == 10 or coin == 25:
+            cents -= coin
 if __name__ == "__main__":
     main()
