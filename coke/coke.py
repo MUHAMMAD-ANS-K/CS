aafb843a01a7758c8951d328e2373093bc598ba0
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-09-27 18:07:13 +0000

    Wed, Sep 27, 2023, 11:07 AM -07:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,5 +5,6 @@ def main():
         coin = int(input("Insert Coin: ").strip())
         if coin == 5 or coin == 10 or coin == 25:
             cents -= coin
+    print(f"Change Owed: {abs(cents)}")
 if __name__ == "__main__":
     main()
