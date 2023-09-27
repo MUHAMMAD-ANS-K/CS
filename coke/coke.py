baa919805003384dc3b396d36dbbfc91beafafd4
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-09-27 17:58:08 +0000

    Wed, Sep 27, 2023, 10:58 AM -07:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,15 +1,4 @@
 def main():
-    total_cents = 50
-    while(total_cents > 0):
-        print(f'Amount Due: {total_cents}')
-        s = get_input()
-        if s == 25 or s == 10 or s == 5:
-            total_cents -= s
-        if(total_cents <= 0):
-            print(f'Change Owed: {abs(total_cents)}')
-def get_input():
-    while True:
-        n = int(input('Insert Coin: '))
-        if n > 0:
-            return n
-main()
+
+if __name__ == "__main__":
+    main()
