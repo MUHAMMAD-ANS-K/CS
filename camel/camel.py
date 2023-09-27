332798b74e5044a71dd793c65623e5239ae2cd69
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2023-09-27 17:56:31 +0000

    Wed, Sep 27, 2023, 10:56 AM -07:00

--- camel/camel.py
+++ camel/camel.py
@@ -2,7 +2,10 @@ def main():
     camel = input("camelCase: ")
     for chr in camel:
         if chr.isupper():
-            print(f"_{chr.lower()}")
+            print(f"_{chr.lower()}",end='')
+        else:
+            print(chr,end='')
+    print()
 
 if __name__ == "__main__":
     main()
