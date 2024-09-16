247d511143f59c9b17e0b7a6a965d1551367f1ae
MUHAMMAD-ANS-K <122625240+MUHAMMAD-ANS-K@users.noreply.github.com>
2024-09-16 06:45:36 +0000

    Sun, Sep 15, 2024, 11:45 PM -07:00

---
+++ lab.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name : Muhammad Ans\nFather : Muhammad Imran\nSchool : Glasgow Public School\nCity : Gujranwala\nMarks : 1079\n");
+}
---
+++ task2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Result = %i\n", 2 + 3 * 5);
+    printf("Result = %i\n", 3 + 5 - 2 * 2);
+    printf("Result = %i\n", 4 / 6 * 3);
+    printf("Result = %i\n", 4 * 3 / 6);
+    printf("Result = %f\n", 4 / 6.0 * 3);
+    printf("Result = %i\n", 8  - 7 % 5);
+    printf("Result = %i\n", 3 + 2 * 4 / 5 - 1 * 6);
+}
